fdfdef844f46c6055d835832b9c005f07f731aba
Rejaur9131 <raju_seu@yahoo.com>
2022-02-17 09:28:34 +0600

    functions used and need more fixationh

--- js/app.js
+++ js/app.js
@@ -1,5 +1,61 @@
+function getInputValue(inputId) {
+  const inputField = document.getElementById(inputId + "-input");
+  const inputAmountText = inputField.value;
+  const amountValue = parseFloat(inputAmountText);
+  return amountValue;
+}
+function updateTotalField(totalFieldId, amount) {
+  const totalElement = document.getElementById(totalFieldId);
+  totalElement.innerText = amount;
+}
+function savings(amount, rate) {
+  return (savingsAmount = amount * (rate / 100));
+}
 document.getElementById("calculate").addEventListener("click", function () {
-  const totalExpensesText = document.getElementById("total-expenses");
+  const incomeAmount = getInputValue("income");
+  //   console.log(incomeTotal);
+  const rentAmount = getInputValue("rent");
+  //   console.log(rentTotal);
+  const foodAmount = getInputValue("food");
+  //   console.log(foodTotal);
+  const otherAmount = getInputValue("others");
+  //   console.log(otherTotal);
+  const totalExpenses = rentAmount + foodAmount + otherAmount;
+  //   console.log(totalExpenses);
+
+  updateTotalField("expenses-amount", totalExpenses);
+
+  //   //   getting and updating total expenses
+  //   const totalExpensesText = document.getElementById("expenses-amount");
+  //   totalExpensesText.innerText = totalExpenses;
+
+  // getting and updating Balance amount
+  const balanceAmount = incomeAmount - totalExpenses;
+  //   const balanceAmountText = document.getElementById("balance-amount");
+  //   balanceAmountText.innerText = balanceAmount;
+  updateTotalField("balance-amount", balanceAmount);
+});
+
+document.getElementById("save").addEventListener("click", function () {
+  const incomeAmount = getInputValue("income");
+  const savingsRateText = getInputValue("percent");
+  const savingsRate = parseFloat(savingsRateText);
+  savings(incomeAmount, savingsRate);
+  updateTotalField("savings-amount", savingsAmount);
+  const balanceAmountText = document.getElementById("balance-amount").innerText;
+  const balanceAmount = parseFloat(balanceAmountText);
+
+  const remainingAmount = balanceAmount - savingsAmount;
+
+  if (savingsAmount > balanceAmount) {
+    updateTotalField("remaining-balance", "Not enough for Savings");
+  } else {
+    updateTotalField("remaining-balance", remainingAmount);
+  }
+});
+
+/* document.getElementById("calculate").addEventListener("click", function () {
+  const totalExpensesText = document.getElementById("expenses-amount");
   const incomeInput = document.getElementById("income-input").value;
   const incomeAmount = parseFloat(incomeInput);
   const foodInput = document.getElementById("food-input").value;
@@ -29,4 +85,4 @@ document.getElementById("save").addEventListener("click", function () {
   const remainingAmount = balanceAmount - savingsAmount;
   const remainingBalanceText = document.getElementById("remaining-balance");
   remainingBalanceText.innerText = remainingAmount;
-});
+}); */
